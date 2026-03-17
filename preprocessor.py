cf45b9ac4375bbb1efeba86661cfa3e9c767da9d
Inragedone <kushkamleshrathod@gmail.com>
2026-03-17 09:59:25 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -4,7 +4,7 @@ import re
 
 def preprocess(data):
     # Regex 
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s?[a-zA-Z]{2}\s-\s'
     messages_raw = re.split(pattern, data)[1:]
     dates_raw = re.findall(pattern, data)
 
